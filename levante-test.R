8ee58bd81af22fd80e6765d1fbf11214e3e031b4
David Cardinal <github@cardinalphoto.com>
2024-11-07 14:26:35 -0800

    Create dependencies

---
+++ levante-test.R
@@ -0,0 +1,10 @@
+# Test script to use Michael Frank's rlevante package
+library(dplyr)
+library(purrr)
+library(rlang)
+library(knitr)
+library(rmarkdown)
+
+# Don't use load as it masks some other functions
+loadNamespace("redivis") 
+
