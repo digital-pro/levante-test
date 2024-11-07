a3f4be8d1090f90981803387ee7945e6448aadc1
David Cardinal <github@cardinalphoto.com>
2024-11-07 14:57:20 -0800

    Reads a data table successfully

--- levante-test.R
+++ levante-test.R
@@ -4,7 +4,18 @@ library(purrr)
 library(rlang)
 library(knitr)
 library(rmarkdown)
+library(rlevante)
 
 # Don't use load as it masks some other functions
+# use redivis::<function> to use its functions
 loadNamespace("redivis") 
+
+# Setup Redivis parameters
+dataset_names <- "DE-pilot"
+org_name <- "levante"
+
+# Retrieve dataset(s)
+our_datasets <- get_datasets(dataset_names, org_name, 'trials') 
+use_dataset <- our_datasets$`DE-pilot`
+trial_table <- use_dataset$trials
 
