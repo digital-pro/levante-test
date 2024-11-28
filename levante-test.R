daac034857c9d698a117bb6853e4ce00a97cee23
David Cardinal <github@cardinalphoto.com>
2024-11-27 18:50:56 -0800

    Have working dataset properties, but with local version of function that should be in rlevante

--- levante-test.R
+++ levante-test.R
@@ -8,6 +8,7 @@ library(httr)
 library(jsonlite)
 
 # Use either the official version or our local version
+devtools::install(pkg = '../rlevante')
 library(rlevante)
 
 # Don't use load as it masks some other functions
@@ -15,38 +16,25 @@ library(rlevante)
 loadNamespace("redivis") 
 
 # Setup Redivis parameters
-dataset_names <- "DE-pilot"
+dataset_name <- "DE-pilot"
 org_name <- "levante"
 
-# Retrieve dataset(s)
-our_datasets <- get_datasets(dataset_names, org_name, 'trials')
-use_dataset <- our_datasets$`DE-pilot`
-trials_table <- use_dataset$trials
-users_table <- use_dataset$users
-
-# clean up ragged tables
-trials_table <- fix_table_types(trials_table)
-tables_to_combine <- list(trials_table, users_table)
-
-all_users <- collect_users(use_dataset)
-
-all_data <- combine_tables(tables_to_combine)
-
-# This is sample code for a client script
-our_datasets <- list_organization_datasets()
-our_datasets_full <- get_datasets_full(dataset_names, org_name)
-found_dataset <- keep(our_datasets_full, name == 'DE-plot')
-dataset_properties <- get_dataset_properties(found_dataset[[1]])
-
-compare_name <- function(dataset) {
-  if (dataset$properties$name == 'DE-pilot')
-    return(TRUE)
-  else
-    return(FALSE)
-  
+# for debugging only
+get_dataset_properties_debug <- function(organization_name, dataset_name) {
+  our_datasets <- list_organization_datasets(organization_name)
+  found_dataset <- lapply(our_datasets, \(ds) {
+    if(ds$name == dataset_name)
+      return(ds) })
+  # cherry pick our dataset -- Should probably be a pipe:)
+  found_dataset <- found_dataset[!found_dataset %in% list(NULL)]
+  dataset_properties <- found_dataset[[1]]$properties
 }
-found_dataset <- our_datasets[sapply(our_datasets, compare_name)]
+
+# Retrieve dataset(s)
+our_dataset <- get_datasets_full(dataset_name, org_name)
+our_dataset_properties <- get_dataset_properties_debug(org_name, dataset_name)
+#trials_table <- get_datasets(dataset_name, org_name, 'trials')
 
 # We want to save a version here for caching
-cache_dataset(use_dataset_full)
+cache_dataset(use_dataset, our_dataset_properties)
 
