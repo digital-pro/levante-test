d539873d6d2a9ec97f1c6dc244bae5234f96dd83
David Cardinal <github@cardinalphoto.com>
2024-11-29 11:25:49 -0800

    knows how to get a dataset & ask for it to be cached

--- levante-test.R
+++ levante-test.R
@@ -19,22 +19,12 @@ loadNamespace("redivis")
 dataset_name <- "DE-pilot"
 org_name <- "levante"
 
-# for debugging only
-get_dataset_properties_debug <- function(organization_name, dataset_name) {
-  our_datasets <- list_organization_datasets(organization_name)
-  found_dataset <- lapply(our_datasets, \(ds) {
-    if(ds$name == dataset_name)
-      return(ds) })
-  # cherry pick our dataset -- Should probably be a pipe:)
-  found_dataset <- found_dataset[!found_dataset %in% list(NULL)]
-  dataset_properties <- found_dataset[[1]]$properties
-}
-
 # Retrieve dataset(s)
 our_dataset <- get_datasets_full(dataset_name, org_name)
-our_dataset_properties <- get_dataset_properties_debug(org_name, dataset_name)
+our_dataset_properties <- get_dataset_properties(org_name, dataset_name)
 #trials_table <- get_datasets(dataset_name, org_name, 'trials')
 
+
 # We want to save a version here for caching
-cache_dataset(use_dataset, our_dataset_properties)
+debug_data <- cache_dataset(our_dataset[[1]], our_dataset_properties)
 
