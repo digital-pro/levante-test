9635dc74c5bed8b9e280dda39610d25739b3d839
David Cardinal <github@cardinalphoto.com>
2024-12-02 16:21:25 -0800

    clean up lint messages

--- levante-test.R
+++ levante-test.R
@@ -10,26 +10,16 @@ library(jsonlite)
 library(tryCatchLog)
 
 # Use either the official version or our local version
-devtools::install(pkg = '../rlevante')
+devtools::install(pkg = "../rlevante")
 library(rlevante)
 
 # Don't use load as it masks some other functions
 # use redivis::<function> to use its functions
-loadNamespace("redivis") 
+loadNamespace("redivis")
 
 # Setup Redivis parameters
 org_name <- "levante"
 tbls <- c("groups", "runs", "trials", "user_groups", "users")
 dataset_names <- c("DE-pilot")
 
-# Get dataset(s)
-#our_dataset <- get_datasets_full(dataset_name, org_name)
-#our_dataset_properties <- rlevante::get_dataset_properties(org_name, dataset_names)
-
-
 our_dataset <- get_datasets(dataset_names, org_name, tables = tbls)
-
-# We _might_ want to save a version here for caching
-#debug_data <- cache_dataset(our_dataset[[1]], our_dataset_properties)
-
-
